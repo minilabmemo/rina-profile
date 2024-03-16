55973f9826b27490611b9182dc8affd94f8f1703
minilabmemo <minilabmemo@gmail.com>
2024-03-16 09:28:50 +0800

    Add: NotFound component

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-
+import NotFound from '../views/NotFound.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    { path: '/:pathMatch(.*)', component: NotFound },
     {
       path: '/',
       name: 'home',
