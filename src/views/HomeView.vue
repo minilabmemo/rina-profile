d52ed14fbcebfd37e27f6da89279d4e69b2c3c21
minilabmemo <minilabmemo@gmail.com>
2024-03-16 09:39:48 +0800

    Fix: test env

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,3 +9,14 @@ import TheWelcome from '../components/TheWelcome.vue'
     <router-link to="./about">go about page</router-link>
   </main>
 </template>
+
+
+<script>
+export default {
+  created() {
+    //查看環境變數，本地跟部屬不同
+    console.log("VITE_API", import.meta.env.VITE_API)
+    console.log("BASE_URL", import.meta.env.BASE_URL)
+  },
+}
+</script>
