eedfd7fafdcf9bbfad7c9a849d3bf52fc592844f
minilabmemo <minilabmemo@gmail.com>
2024-04-24 09:24:47 +0800

    Add: width test

--- src/views/Base/AboutView.vue
+++ src/views/Base/AboutView.vue
@@ -36,11 +36,101 @@
         <span class="text-custom-color "> custom-color</span>
       </div>
     </div>
+    <div class="w-75">
+      <div class="">
+        <h5>width in content-box</h5>
+        <ul>
+          <li>外層 a：100 width *100 height padding 5px;border:5px</li>
+          <li>內層 b：100％ width *100％ height padding 10px;border:10px</li>
+        </ul>
+      </div>
+      <div class=" ">
+
+        <div class="box-a">a
+        </div>
+        <div class="box-b">b</div>
+        <div class="box-a">
+          <div class="box-b">a contain b</div>
+        </div>
+      </div>
+      <div>
+        結果：
+        <ul>
+          <li>內層 100% =100px</li>
+          <li>但是內層一但有了自己的 border/padding 就會溢出</li>
+          <li>內層加了 margin 也會溢出</li>
+        </ul>
+      </div>
+      <div class="">
+        <h5>width in border-box </h5>
+        <ul>
+          <li>外層</li>
+        </ul>
+      </div>
+      <div class=" ">
+
+        <div class="box-c">c
+        </div>
+        <div class="box-d">d</div>
+        <div class="box-c">
+          <div class="box-d">c contain d</div>
+        </div>
+        <div>
+          結果：
+          <ul>
+            <li>外層 100px*100px 內容 80px * 80px 內層寬高設定也變為 80px*80px 只不過內容是 40px*40px</li>
+            <li> <code>所以 100% 是對應實際內容寬高，並非設定好的寬高</code></li>
+            <li>不會溢出，但字數還是可能溢出，要注意</li>
+            <li><code> 這是因為 border-box 計算的功用，但是不包含加上 margin 如果子元素加上 margin 還是會爆出來。</code></li>
+          </ul>
+        </div>
+      </div>
+    </div>
   </div>
+
 
 </template>
 
 <style>
+.box-a {
+  box-sizing: content-box;
+  width: 100px;
+  height: 100px;
+  padding: 5px;
+  border: 5px black solid;
+  background-color: blue;
+}
+
+.box-b {
+  box-sizing: content-box;
+  width: 100%;
+  height: 100%;
+  padding: 10px;
+  border: 10px pink solid;
+
+  background-color: yellow;
+}
+
+.box-c {
+  box-sizing: border-box;
+  width: 100px;
+  height: 100px;
+  padding: 5px;
+  border: 5px black solid;
+  background-color: blue;
+}
+
+.box-d {
+  box-sizing: border-box;
+  width: 100%;
+  height: 100%;
+  padding: 10px;
+  border: 10px pink solid;
+
+  background-color: yellow;
+}
+
+
 @media (min-width: 1024px) {
   .about {
     min-height: 100vh;
