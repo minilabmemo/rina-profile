0ab8bfda01914815f7aa0905d9f5cb5a54b1caa9
minilabmemo <minilabmemo@gmail.com>
2024-05-03 11:26:12 +0800

    Fix: img config

--- src/utils/config/works.ts
+++ src/utils/config/works.ts
@@ -9,13 +9,18 @@ export interface Iwork {
   details: string[]
   isHide?: boolean
 }
-import imgUrl from '../../assets/images/work-ec.png'
+import imgEc from '../../assets/images/work-ec.png'
+import imgMeow from '@/assets/images/work-meow.png'
+import imgBus from '@/assets/images/work-bus.png'
+import imgTW from '@/assets/images/work-tw.png'
+import imgMiao from '@/assets/images/work-miao.png'
+import imgEBook from '@/assets/images/work-e-book.png'
 
 export const works: Iwork[] = [
   {
     date: '2024/05',
     title: '電子商務購物網站',
-    img: imgUrl,
+    img: imgEc,
     tags: ['vue', 'bootstrap', 'pinia', 'vite', 'eslint', 'cypress'],
     website: 'https://minilabmemo.github.io/f2e-ec-store/#/',
     repository: 'https://github.com/minilabmemo/f2e-ec-store',
@@ -24,7 +29,7 @@ export const works: Iwork[] = [
   {
     date: '2024/01',
     title: '薩卡班甲魚官網 ',
-    img: '/src/assets/images/work-meow.png',
+    img: imgMeow,
     tags: ['vue', 'typescript', 'tailwind', 'storybook'],
     website: 'https://minilabmemo.github.io/f2e-showcase-vite/meow/',
     repository: 'https://github.com/minilabmemo/f2e-showcase-vite',
@@ -37,7 +42,7 @@ export const works: Iwork[] = [
   {
     date: '2023/11',
     title: '公車動態查詢服務 ',
-    img: '/src/assets/images/work-bus.png',
+    img: imgBus,
     tags: ['react', 'typescript', 'scss', 'Leaflet', 'OpenStreetMap'],
     website: 'https://minilabmemo.github.io/f2e-time-twbus/',
     repository: 'https://github.com/minilabmemo/f2e-time-twbus',
@@ -47,7 +52,7 @@ export const works: Iwork[] = [
   {
     date: '2023/11',
     title: '薩卡班甲魚官網 ',
-    img: '/src/assets/images/work-meow.png',
+    img: imgMeow,
     tags: ['react', 'typescript', 'tailwind'],
     website: 'https://minilabmemo.github.io/f2e-election-meowsakka/',
     repository: 'https://github.com/minilabmemo/f2e-election-meowsakka',
@@ -59,7 +64,7 @@ export const works: Iwork[] = [
   {
     date: '2023/12',
     title: '即時開票全台地圖 ',
-    img: '/src/assets/images/work-tw.png',
+    img: imgTW,
     tags: ['react', 'next', 'typescript', 'tailwind', 'D3', 'vercel'],
     website: 'https://f2e-president-tw.vercel.app/',
     repository: 'https://github.com/minilabmemo/f2e-president-tw',
@@ -72,7 +77,7 @@ export const works: Iwork[] = [
   {
     date: '2023/11',
     title: '喵立漢競選官網 ',
-    img: '/src/assets/images/work-miao.png',
+    img: imgMiao,
     tags: ['react', 'typescript', 'scss'],
     website: ' https://minilabmemo.github.io/f2e-election-person/',
     repository: ' https://github.com/minilabmemo/f2e-election-person',
@@ -84,7 +89,7 @@ export const works: Iwork[] = [
   {
     date: '2023/10',
     title: '電子書關鍵字速查 ',
-    img: '/src/assets/images/work-e-book.png',
+    img: imgEBook,
     tags: ['chrome extension', 'css', 'javascript'],
     repository: ' https://github.com/minilabmemo/chrome-ext-find-ebook',
     details: ['Chrome 擴充功能插件', '快速找尋電子書關鍵字', '可自訂網址']
