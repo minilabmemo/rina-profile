0beb71dafb4def993fe7b13b675fdd0f0f8e63e5
minilabmemo <minilabmemo@gmail.com>
2024-05-21 18:06:28 +0800

    💫 Add: workItem with rotate transform

--- src/components/Lab/WorkItem.vue
+++ src/components/Lab/WorkItem.vue
@@ -1,63 +1,106 @@
 <template>
-  <div class="d-flex flex-column align-items-center  justify-content-center p-3 work-item m-3">
-    <div class="text-primary fw-bolder fs-5 ">{{ item.date }}</div>
-    <div class="bg-primary  title-line " style="height: 5px;"></div>
-    <div class=" content-item mt-1 p-3 d-flex flex-column align-items-center ">
-      <div class="text-black fs-4 fw-bolder my-2  ">{{ item.title }}</div>
-      <div class="" style="max-width: 200px"> <img :src="item.img" alt="work" class="flex-image ">
-      </div>
-      <div class="d-flex   flex-wrap align-items-center   gap-2 my-3 " style="height: 3rem">
-        <span style="min-width: 3rem" :class="tagColor(tag)" class="px-2  fw-bolder text-white text-center "
-          v-for="tag in item.tags" :key="tag">{{ tag }}</span>
-      </div>
+  <div class="card-container" @mousemove="handleMouseMove" @mouseleave="handleMouseLeave">
+    <div
+      :style="cardStyle"
+      class="card d-flex flex-column align-items-center justify-content-center p-3 work-item m-3"
+    >
+      <div class="text-primary fw-bolder fs-5">{{ item.date }}</div>
+      <div class="bg-primary title-line" style="height: 5px"></div>
+      <div class="content-item mt-1 p-3 d-flex flex-column align-items-center">
+        <div class="text-black fs-4 fw-bolder my-2">{{ item.title }}</div>
+        <div class="" style="max-width: 200px">
+          <img :src="item.img" alt="work" class="flex-image" />
+        </div>
+        <div class="d-flex flex-wrap align-items-center gap-2 my-3" style="height: 3rem">
+          <span
+            style="min-width: 3rem"
+            :class="tagColor(tag)"
+            class="px-2 fw-bolder text-white text-center"
+            v-for="tag in item.tags"
+            :key="tag"
+            >{{ tag }}</span
+          >
+        </div>
 
-      <div class="align-self-start fw-bolder " style="height: 5.2rem">
-        <ul>
-          <li class="" v-for="(detail, i) in item.details" :key="i">{{ detail }}</li>
-        </ul>
-      </div>
-      <div class="w-100 d-flex justify-content-evenly     mt-1 fs-5 fw-bolder ">
-        <span class="nav-link" v-if="item.website"> <a :href="item.website" class="text-secondary " target="_blank"> <i
-              class="bi bi-globe"></i>
-            website</a> </span>
-        <span class=""> <a :href="item.repository" class="text-secondary" target="_blank">
-            <i class="bi bi-github"></i>
-            repository</a>
-        </span>
+        <div class="align-self-start fw-bolder" style="height: 5.2rem">
+          <ul>
+            <li class="" v-for="(detail, i) in item.details" :key="i">{{ detail }}</li>
+          </ul>
+        </div>
+        <div class="w-100 d-flex justify-content-evenly mt-1 fs-5 fw-bolder">
+          <span class="nav-link" v-if="item.website">
+            <a :href="item.website" class="text-secondary" target="_blank">
+              <i class="bi bi-globe"></i> website</a
+            >
+          </span>
+          <span class="">
+            <a :href="item.repository" class="text-secondary" target="_blank">
+              <i class="bi bi-github"></i>
+              repository</a
+            >
+          </span>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
-
-
 <script setup lang="ts">
-import { defineProps } from 'vue';
+import { defineProps, ref } from 'vue'
 import { tagColors } from '@/utils/config/works'
-import type { Iwork } from '@/utils/config/works';
+import type { Iwork } from '@/utils/config/works'
 
 const props = defineProps<{
-  item: Iwork;
-}>();
+  item: Iwork
+}>()
 
 function tagColor(tag: string): string {
-  tag = tag.toLowerCase().replace(" ", "")
+  tag = tag.toLowerCase().replace(' ', '')
   if (tag in tagColors) {
-    return tagColors[tag];
+    return tagColors[tag]
   } else {
-    return 'bg-gray';
+    return 'bg-gray'
+  }
+}
+const cardStyle = ref({
+  transform: 'rotateY(0deg) rotateX(0deg)'
+})
+function handleMouseMove(event) {
+  console.log('handleMouseMove')
+
+  const card = event.currentTarget.querySelector('.card')
+  const rect = card.getBoundingClientRect()
+  const x = event.clientX - rect.left
+  const y = event.clientY - rect.top
+  const halfWidth = rect.width / 2
+  const halfHeight = rect.height / 2
+  const rotateX = ((y - halfHeight) / halfHeight) * 5
+  const rotateY = ((x - halfWidth) / halfWidth) * 5
+
+  cardStyle.value = {
+    transform: `rotateY(${rotateY}deg) rotateX(${-rotateX}deg)`
   }
 }
 
-
-
+function handleMouseLeave(event) {
+  cardStyle.value = {
+    transform: 'rotateY(0deg) rotateX(0deg)'
+  }
+}
 </script>
 
-
 <style lang="css" scoped>
+.card-container {
+  perspective: 1000px;
+}
+
+.card {
+  box-shadow: 0 10px 20px rgba(142, 142, 142, 0.2);
+  transform-style: preserve-3d;
+  transition: transform 0.1s ease-out;
+}
 .img-container {
   overflow: hidden;
-
 }
 
 .work-item {
@@ -69,14 +112,13 @@ function tagColor(tag: string): string {
 }
 
 .title-line {
-  transition: width .5s ease-in;
+  transition: width 0.5s ease-in;
   width: 0px;
 }
 
 .work-item:hover .title-line {
   width: 100px;
 }
-
 
 .work-item:hover img {
   transform: scale(1.1);
